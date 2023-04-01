0acfa1396de06feaddb6f53d3aaf6414467d1c3c
Nicholas <nicholasdev@zohomail.com>
2023-04-01 13:13:25 -0300

    Exercício finalizado

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//Made by Nicholasdev
+
+//Declaração das variáveis
+double valor1, valorpago, troco;
+
+//Pede o valor da compra para o usuário
+Console.WriteLine("Digite o valor da compra");
+valor1 = double.Parse(Console.ReadLine()!);
+
+//Pede o valor pago para o usuário
+Console.WriteLine("Digite o valor pago");
+valorpago = double.Parse(Console.ReadLine()!);
+
+//Subtrai o valor da compra pelo valor pago
+troco = valorpago - valor1;
+
+//Exibe o troco do usuário
+Console.WriteLine($"O seu troco é de: {troco}");
